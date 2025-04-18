1ad14b6297f41bc7ba0a08b718e0109c5c349590
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-17 23:13:04 -0600

    Add categories buttons to search var

---
+++ src/db/categories.ts
@@ -0,0 +1,27 @@
+export default {
+  projects: [
+    "javascript",
+    "codigo limpio",
+    "typescript",
+    "rust",
+    "c",
+    "python",
+    "java",
+    "react",
+    "astro",
+    "bajo nivel",
+    "sistemas operativos",
+    "linux",
+    "desarrollo web",
+  ],
+  works: [
+    "sistemas embebidos",
+    "desarrollo web",
+    "desarrollo de aplicaciones móviles",
+  ],
+  blog: [
+    "codigo limpio",
+    "desarrollo web",
+    "desarrollo de aplicaciones móviles",
+  ],
+};
